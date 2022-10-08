7a03b586a4cb207bf8d9509121153c570b4998fa
l5769389 <5769389@qq.com>
2022-10-08 16:21:23 +0800

    feat: 123

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import installElementPlus from './plugins/element'
+import installIcons from '@/icons'
+const app = createApp(App)
+installElementPlus(app)
+installIcons(app)
+app.use(store).use(router).mount('#app')
